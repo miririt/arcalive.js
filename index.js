b334c48c850ee8598fc0232fe56e780457d62e6a
miririt <eliotthescarlet@gmail.com>
2021-02-10 22:43:54 +0900

    Foodoc은 devDependency였지;

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const RequestSession = require('./src/request-session');
 const Board = require('./src/board');
 const Article = require('./src/article');
 const Comment = require('./src/comment');
-const fq = require('./src/fetch-queue');
+const FetchQueue = require('./src/fetch-queue');
 
-fq.setRateLimit(400);
+FetchQueue.setRateLimit(1000);
 
 module.exports = {
+  FetchQueue: FetchQueue,
   Session: RequestSession,
   Board: Board,
   Article: Article,
