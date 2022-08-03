1c55693982adf42a0b8aa1442a2b265ed1d39b72
miririt <eliotthescarlet@gmail.com>
2022-08-03 15:29:10 +0900

    README와 test 일부 수정

--- README.md
+++ README.md
@@ -1,64 +1,59 @@
-
 # Usage
 
 ## 1. 세션 만들기
 
 ```javascript
-const Arca = require('arcalive');
+const Arca = require("arcalive");
 
-const session = new Arca.Session(username, password); // username과 password로 아이디와 비밀번호 전달
+const session = await Arca.Session.login(username, password); // username과 password로 아이디와 비밀번호 전달
 ```
 
 ## 2. 원하는 게시판(채널) 열기
 
 ```javascript
-const board = await session.getBoard('breaking');
+const board = await session.getBoard("breaking");
 ```
 
 ### 2-1. 게시판에 대해서 원하는 조작 수행
 
 ```javascript
-
 board.readArticle(articleId); // 해당 board에서 articleId에 해당하는 게시글을 읽어옴.
-board.writeArticle({ // 해당 board에 게시글을 작성함
-  category: '정보',
-  title: '게시글 작성',
-  content: '테스트 내용입니다.<br>HTML 코드를 전달합니다.'
+board.writeArticle({
+  // 해당 board에 게시글을 작성함
+  category: "정보",
+  title: "게시글 작성",
+  content: "테스트 내용입니다.<br>HTML 코드를 전달합니다.",
 });
 board.deleteArticle(articleId); // 해당 board에서 articleId에 해당하는 게시글을 삭제함
-board.editArticle(articleId, { // 해당 게시글의 내용을 수정함
-  category: '정보',
-  title: '게시글 작성',
-  content: '테스트 내용입니다.<br>HTML 코드를 전달합니다.'
+board.editArticle(articleId, {
+  // 해당 게시글의 내용을 수정함
+  category: "정보",
+  title: "게시글 작성",
+  content: "테스트 내용입니다.<br>HTML 코드를 전달합니다.",
 });
 
 board.readPage(pageNo); // 해당 board에서 pageNo번째 페이지에 있는 게시글들의 미리보기를 읽어옴(인덱스는 1부터 시작)
-
 ```
 
 ## 3. 원하는 게시글 열기
 
 ```javascript
-
 const article = board.getArticle(articleId); // 해당 board에서 articleId에 해당하는 객체를 얻어옴
-
 ```
 
 ### 3-1. 게시글에 대해서 원하는 조작 수행
 
 ```javascript
-
 article.read(); // 게시판에서 수행하는 작업과 동일
 article.delete();
 article.edit({
-  category: '정보',
-  title: '게시글 작성',
-  content: '테스트 내용입니다.<br>HTML 코드를 전달합니다.'
+  category: "정보",
+  title: "게시글 작성",
+  content: "테스트 내용입니다.<br>HTML 코드를 전달합니다.",
 });
 
 article.writeComment(comment); // 해당 게시글에 댓글을 작성함
 article.deleteComment(commentId); // 해당 게시글
-
 ```
 
 모든 조작은 Promise로 반환됩니다.
--- test/index.js
+++ test/index.js
@@ -1,45 +1,39 @@
-const Arca = require('..');
+const Arca = require("..");
 
-describe('익명 세션 게시글 열람 테스트', function() {
+describe("익명 세션 게시글 열람 테스트", function () {
   const session = Arca.Session.anonymousSession();
 
-  it('getBoard로 게시글 열람 테스트', async function() {
-    const board = await session.getBoard('notice');
+  it("getBoard로 게시글 열람 테스트", async function () {
+    const board = await session.getBoard("notice");
     return await board.readArticle(6457546, {
       noCache: true,
-      withComments: false
+      withComments: false,
     });
   });
 
-  it('fromUrl로 채널을 통한 게시글 열람 테스트', async function() {
-    const board = await session.fromUrl('https://arca.live/b/notice');
+  it("fromUrl로 채널을 통한 게시글 열람 테스트", async function () {
+    const board = await session.fromUrl("https://arca.live/b/notice");
     return await board.readArticle(6457546, {
       noCache: true,
-      withComments: false
+      withComments: false,
     });
   });
 
-  it('fromUrl로 직접 게시글 열람 테스트', async function() {
-    const article = await session.fromUrl('https://arca.live/b/notice/6457546');
+  it("fromUrl로 채널을 통한 댓글 열람 테스트", async function () {
+    const board = await session.fromUrl("https://arca.live/b/notice");
+    const article = await board.readArticle(6457546, {
+      noCache: true,
+      withComments: true,
+    });
+    const [anyComment] = article.comments;
+    await anyComment.read();
+  });
+
+  it("fromUrl로 직접 게시글 열람 테스트", async function () {
+    const article = await session.fromUrl("https://arca.live/b/notice/6457546");
     return await article.read({
       noCache: true,
-      withComments: true
+      withComments: true,
     });
-  });
-});
-
-describe('특수 도메인 테스트', function() {
-  const session = Arca.Session.anonymousSession();
-
-  it('getBoard로 특수 도메인 테스트', async function() {
-    const specialBoard = await session.getBoard('smpeople');
-    const firstPage = await specialBoard.readPage(1);
-    return await firstPage[0].read();
-  });
-
-  it('fromUrl로 특수 도메인 테스트', async function() {
-    const specialBoard = await session.fromUrl('https://sm.arca.live/b/smpeople');
-    const firstPage = await specialBoard.readPage(1);
-    return await firstPage[0].read();
   });
 });
